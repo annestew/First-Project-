3889a7090639fd07639dc692d26e873f3a3535a6
bethdianethomas <bethdianethomas@yahoo.com>
2015-09-29 14:30:48 -0600

    latest commit

--- jave.js
+++ jave.js
@@ -5,4 +5,13 @@ myFunction = function() {
 }
 
 
+var batMan = "<img src='http://i.imgur.com/8SL7lsZ.png' class='img' alt='Batdog' id='batDog'>";
+var xLevel = 2;
+var yLevel = 0; 
 
+var moveBatman = function () {
+	document.getElementById(xLevel.toString() + yLevel.toString()).innerHTML=batMan;
+}
+
+
+moveBatman();
