7c56c4ffcd84abea19d1d3dc49587b7affc66b93
Annie <AnneStew@Annies-MacBook-Air.local>
2015-10-04 13:37:27 -0600

    Making a new grid as well as trying to add borders

--- jave.js
+++ jave.js
@@ -1,47 +1,147 @@
-
+ 
 myFunction = function() {
-
 	var value = document.getElementById("firstInput").value;
+	// var endOfWorld = endOfWorldTest();
+	// alert (xLevel)
 
-	 
-	if (value === "down") {
+	if (endOfWorld = false) {
+		alert("oh no");
+	}
+
+	else if (value === "up") {
+		movement(value); 
+	}
+
+	else if (value==="right"){
+		movement(value); 
+	}
+
+	else if (value === "down"){
+		movement(value); 
+	}
+
+	else if (value=== "left"){
+		movement(value);
+	}
+
+	else {
+		alert("Try Entering Left, Right, Down or Up");
+	}
+	
+}
+
+//Create a new function to test for errors
+//If x is too far then condition is true else it is false
+//If y is too far then condition is true else it is false
+// function endOfWorldTest(endOfWorld) {
+var endOfWorld = function () {
+
+if ((xLevel==2) && (xChange==1)){
+	return false;
+}
+
+else if ((xLevel== 0) && (xChange== -1)){
+	return false;
+}
+
+else  if ((yLevel== 2) && (yChange== 1)){
+	return false;
+}
+
+else if ((yLevel== 0) && (yChange == -1)){
+	return false;
+}
+else {
+	return true;
+}
+}
+
+
+
+
+
+// var tooMuch = function (changeX, changeY) {
+    
+//     if (changeY + yLevel == 1){
+//         alert("TooFar");
+//         return true;
+//     } else {
+//         return false;
+//     }
+
+// }
+
+function movement(direction) {
+// if (xLevel ===2) {
+//    	TooMuch();
+//    }
+// else
+
+
+//   if (yLevel ===2) {
+//    	TooMuch();
+//    }
+//    else
+
+	if (direction === "down") {
 		clearBatman();
 		yLevel++;
 		moveBatman();
 		
 	}
 
-	if (value === "up") {
+	if (direction === "up") {
 		clearBatman();
 		yLevel--;
 		moveBatman();
 
 	}
 
-	if (value === "right") {
+	if (direction === "right") {
 		clearBatman();
 		xLevel++;
 		moveBatman();	
 		
 	}
-	if (value === "left") {
+	if (direction === "left") {
 		clearBatman();
 		xLevel--;
 		moveBatman();
 	}
 
-   if (xLevel === 3) {
-   	TooMuch();
-
-   }
 	
+//////get y level for too much(); 
+}
+//////clean up arrow function 
+
+function arrowfunction(e) {
+	// alert("oh no yo")
+	var e = event.keyCode;
+
+    if (e == 40) { //down function
+        movement("down")
+
+    } else if (e == 37) { //left function
+        clearBatman();
+		xLevel--;
+		moveBatman();
+    } else if (e == 39) { //right function
+       	clearBatman();
+		xLevel++;
+		moveBatman();
+    } else if (e == 38) { //up function
+        clearBatman();
+		yLevel--;
+		moveBatman();
+    }
+
 }
 
-
+var endOfWorld = false;
 var newImage = "";
 
 
-var batMan = "<img src='http://i.imgur.com/8SL7lsZ.png' class='img' alt='Batdog' id='batDog'>";
+var batMan = "<img src='images/funny-dog.jpg' class='img' alt='Batdog' id='batDog'>";
 var xLevel = 0;
 var yLevel = 0; 
 
@@ -55,6 +155,8 @@ var clearBatman = function () {
 var TooMuch = function() {
 	alert("Move Back To Where You Came From");
 }
+
+document.onkeydown= arrowfunction;
 
 moveBatman();
 
